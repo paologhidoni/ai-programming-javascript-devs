d3dc23c555f7e4e67433fd1fafcf673efa7ff50b
paologhidoni <paolo.ghidoni82@gmail.com>
2026-03-02 16:11:23 +0100

    Use readline to create a CLI in the terminal to ask questions to AI

--- 03/index.js
+++ 03/index.js
@@ -1,5 +1,11 @@
+/**
+ * Using Readline we can create a CLI in the terminal which allows us to create an interface
+ * to ask questions and get answers.
+ */
+
 import dotenv from "dotenv";
 import OpenAI from "openai";
+import readline from "readline";
 
 // Load environment variables from .env.backend file
 dotenv.config({ path: ".env.backend" });
@@ -8,34 +14,23 @@ dotenv.config({ path: ".env.backend" });
 const openAIKey = process.env.OPENAI_API_KEY;
 const openaiClient = new OpenAI({ apiKey: openAIKey });
 
-async function getGameInfo(game) {
-  const stream = await openaiClient.chat.completions.create({
+const rl = readline.createInterface({
+  input: process.stdin, // what the user types in
+  output: process.stdout, // what the user receives back
+});
+
+rl.question("What do you want to ask the robots?", async (question) => {
+  let res = await openaiClient.chat.completions.create({
     model: "gpt-4o-mini",
     messages: [
       {
         role: "user",
-        content: `For this videogame: ${game}, provide the following information structured as bulletpoints: 
-        - Title
-        - Year of release
-        - Published by
-        - Developed by
-        - Game genre
-        - Multiplayer options
-        - Short Description
-        `,
+        content: question,
       },
-      { role: "system", content: "You are a videogames history expert" },
+      { role: "system", content: "You are a friendly robot!" },
     ],
-    // Stream true allows to get access to the stream, to then be able to use chunks of it as they
-    // are available instead of waitng for all of it
-    stream: true,
-    // Determines the length of the response
-    // max_tokens: 64,
   });
 
-  for await (const chunk of stream) {
-    process.stdout.write(chunk.choices[0].delta.content || "");
-  }
-}
-
-getGameInfo("Tomb Raider");
+  console.log(res.choices[0].message);
+  rl.close();
+});
